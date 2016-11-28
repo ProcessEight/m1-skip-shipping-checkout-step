be7ec5c782578828fd1704f709fff2e2f53fc476
Simon Frost <s.frost@frostnet.co.uk>
2016-11-28 11:08:34 +0000

    MDCMK92: Created module skeleton; Added surcharge field to ccsave payment method

---
+++ src/app/code/local/Sfrost2004/MDCMK92PaymentMethodSurcharge/Helper/Data.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Zone8
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact Zone8 for more information.
+ *
+ * @category    Zone8
+ * @package     Default (Template) Project
+ * @copyright   Copyright (c) 2016 Zone8
+ * @author      Zone8
+ *
+ */ 
+class Sfrost2004_MDCMK92PaymentMethodSurcharge_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
